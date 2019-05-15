e5d7723a6d0ac4a7e3ef8c6bf1119ea94b26da07
JPantana <joshpantana@me.com>
2019-05-14 20:45:40 -0500

    wip

---
+++ src/javascripts/components/pins/pins.js
@@ -0,0 +1,12 @@
+// import util from '../../helpers/util';
+import pinsData from '../../helpers/data/pinsData';
+
+const initPins = (boardId) => {
+  pinsData.loadPinsForBoard(boardId)
+    .then((pins) => {
+      console.error('all pins', pins);
+    })
+    .catch(err => console.error(err));
+};
+
+export default { initPins };
