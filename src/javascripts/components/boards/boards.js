1f4376753790873b7bf6da130acd69a6652bb543
JPantana <joshpantana@me.com>
2019-05-14 19:24:39 -0500

    boards printing

--- src/javascripts/components/boards/boards.js
+++ src/javascripts/components/boards/boards.js
@@ -1,15 +1,30 @@
-// import util from '../../helpers/util';
+import util from '../../helpers/util';
 import boardsData from '../../helpers/data/boardsData';
+import './boards.scss';
 
-// const boardsArr = [];
+let writeBoards = [];
+
+const boardDomBuilder = () => {
+  let domString = '';
+  writeBoards.forEach((board) => {
+    domString += `
+    <div class="col-3">
+      <div id="${board.id}" class="mt-2 p-2 card border-danger justify-content-center" style="height: 100px;">
+        <p class="row text-center m-auto">${board.name}</p>
+      </div>
+    </div>`;
+  });
+  util.printToDom('user-boards', domString);
+};
 
 const initBoards = () => {
-  console.error('hi from boards');
   boardsData.loadBoards()
     .then((resp) => {
-      console.error(resp.data.boards);
+      const dataResults = resp.data.boards;
+      writeBoards = dataResults;
+      boardDomBuilder();
     })
     .catch(err => console.error(err));
 };
 
-export default { initBoards };
+export default { initBoards, boardDomBuilder };
